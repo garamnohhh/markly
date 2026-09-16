2b0951bc07d6d15f4494e684fd2b3eabfb8911b4
garamnoh <204321291+garamnohhh@users.noreply.github.com>
2026-09-16 15:07:42 +0900

    feat: Tauri 업데이트 플러그인 연결

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -11,6 +11,8 @@ pub fn run() {
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_clipboard_manager::init())
+        .plugin(tauri_plugin_updater::Builder::new().build())
+        .plugin(tauri_plugin_process::init())
         .manage(VaultState::default())
         .setup(|app| {
             #[cfg(target_os = "macos")]
